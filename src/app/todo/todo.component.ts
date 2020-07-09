6085cc4b779376c15b6f9281f06fef988518a2cf
douglaschu <douglas.l.chu@gmail.com>
2020-07-08 22:16:55 -0400

    fourth commit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/todo';
-import { TODO } from '../todo-list';
+import { TODOS } from '../todo-list';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
+
 export class TodoComponent implements OnInit {
-  todos = TODO;
+  
+  todos = TODOS;
   selectedTodo: Todo;
 
-  onSelect(todo: Todo): void {
-    this.selectedTodo = todo;
-  }
+  private isButtonVisible = true;  
   
   constructor() { }
 
